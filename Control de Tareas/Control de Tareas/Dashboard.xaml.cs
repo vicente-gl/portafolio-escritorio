16122880b1236aa239d9ec996e30d1ccea15cbc7
vicente-gl <vice.gonzalezl@duocuc.cl>
2022-09-22 00:17:06 -0300

    Funciones Botones y bugfix menu2

--- Control de Tareas/Control de Tareas/Dashboard.xaml.cs
+++ Control de Tareas/Control de Tareas/Dashboard.xaml.cs
@@ -17,41 +17,86 @@ namespace Control_de_Tareas
     /// <summary>
     /// Interaction logic for Dashboard.xaml
     /// </summary>
+    /// 
+
     public partial class Dashboard : Window
     {
+        string color_menu1_idle = "#FFCFCFCF";
+        string color_menu1_pressed = "#EDEDED";
+        string color_menu2_idle = "#EDEDED";
+        string color_menu2_pressed = "#FFFFFF";
+
+        bool menu1_selected = false;
+        bool menu2_selected = false;
+
+
         public Dashboard()
         {
             InitializeComponent();
             WindowState = WindowState.Maximized;
         }
 
+        //Botones Main Menú
         private void mainMenuNegocios_Click(object sender, RoutedEventArgs e)
         {
-            if (MenuNegocio.Visibility.Equals(Visibility.Hidden))
+            OcultarMenu2();
+            ApagarBotonesMainMenu();
+            if (MenuNegocio.Visibility.Equals(Visibility.Hidden) && !menu1_selected)
             {
                 MenuNegocio.Visibility = Visibility.Visible;
+                CambiarColorBoton(mainMenuNegocios, color_menu1_pressed);
+                menu1_selected = true;
             }
             else
             {
-                MenuNegocio.Visibility = Visibility.Hidden;
-
+                menu1_selected = false;
             }
         }
 
+        private void mainMenuGruposTrabajo_Click(object sender, RoutedEventArgs e)
+        {
+            OcultarMenu2();
+            ApagarBotonesMainMenu();
+            if (MenuNegocio.Visibility.Equals(Visibility.Hidden) && !menu1_selected)
+            {
+                CambiarColorBoton(mainMenuGruposTrabajo, color_menu2_pressed);
+                menu2_selected = true;
+            }
+            else
+            {
+                menu2_selected = false;
+            }
+        }
+
+
+
+
+        //Botones Menu Negocio
         private void btn_negocio_crear_Click(object sender, RoutedEventArgs e)
         {
-            if (Pantalla_Agregar_Negocio.Visibility.Equals(Visibility.Hidden))
+            if (Pantalla_Agregar_Negocio.Visibility.Equals(Visibility.Hidden) && !menu2_selected)
             {
                 date_pick.SelectedDate = DateTime.Now;
                 Pantalla_Agregar_Negocio.Visibility = Visibility.Visible;
+
+                CambiarColorBoton(btn_negocio_crear, color_menu2_pressed);
+
+                menu2_selected = true;
+
             }
             else
             {
                 Pantalla_Agregar_Negocio.Visibility = Visibility.Hidden;
 
+                var bc = new BrushConverter();
+                CambiarColorBoton(btn_negocio_crear, color_menu2_idle);
+
+                menu2_selected = false;
+
             }
         }
 
+        //Botones Crear Negocio
         private void btn_crearNegocio_limpiar_Click(object sender, RoutedEventArgs e)
         {
             txtbox_nombre.Text = "";
@@ -63,6 +108,30 @@ namespace Control_de_Tareas
             txtbox_web_rrss.Text = "";
             txtbox_girocomercial.Text = "";
             date_pick.SelectedDate = DateTime.Now;
+        }
+
+
+        //Metodos Extras
+        private void CambiarColorBoton(Button botonObjetivo, string nuevo_color)
+        {
+            var bc = new BrushConverter();
+            botonObjetivo.Background = (Brush)bc.ConvertFrom(nuevo_color);
+        }
+
+        private void ApagarBotonesMainMenu()
+        {
+            CambiarColorBoton(mainMenuNegocios, color_menu1_idle);
+            CambiarColorBoton(mainMenuGruposTrabajo, color_menu1_idle);
+            CambiarColorBoton(mainMenuFlujosTarea, color_menu1_idle);
+            CambiarColorBoton(mainMenuUsuarios, color_menu1_idle);
+            CambiarColorBoton(mainMenuPerfilesNegocio, color_menu1_idle);
+        }
+
+        private void OcultarMenu2()
+        {
+            MenuNegocio.Visibility = Visibility.Hidden;
+            MenuGrupoTrabajo.Visibility = Visibility.Hidden;
+            
         }
     }
 }
